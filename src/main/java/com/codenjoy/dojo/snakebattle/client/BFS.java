51b7c02dbe68ca6af1662798bd9da8c5812b05c6
Illya Havsiyevych <illya.havsiyevych@gmail.com>
2019-02-02 08:25:45 +0200

    BFS refactoring

--- src/main/java/com/codenjoy/dojo/snakebattle/client/BFS.java
+++ src/main/java/com/codenjoy/dojo/snakebattle/client/BFS.java
@@ -35,14 +35,28 @@ import static com.codenjoy.dojo.snakebattle.model.Elements.NONE;
 
 
 public class BFS {
-    public static Optional<Direction> bfs(Board board, Point start, boolean weight, Elements[] barrier, Elements[] target, int size, MODE mode) {
+    private Board board;
+    private Point start;
+    private boolean weight;
+    private MODE mode;
+    private Elements[] barrier;
+    private Elements[] target;
+
+    private BFS(Board board, Point start) {
+        this.board = board;
+        this.start = start;
+        weight = false;
+        mode = MODE.NORMAL;
+    }
+
+    public Optional<Direction> bfs(int size) {
         Queue<Point> queue = new LinkedList<>();
         queue.add(start);
 
         Map<Point, Path> visited = new HashMap<>(size*2);
         visited.put(start, new Path(null, null, 0));
 
-        LinkedHashSet<Point> found = bfs(board, queue, visited, barrier, target, size, mode, weight);
+        LinkedHashSet<Point> found = bfs(queue, visited, size);
         if (found.isEmpty())
             return Optional.empty();
 /*
@@ -89,21 +103,21 @@ public class BFS {
         return Optional.of(sorted.get(0));
     }
 
-    private static Direction traceBack(Point start, Point point, Map<Point, Path> visited ) {
+    private Direction traceBack(Point start, Point point, Map<Point, Path> visited ) {
         while (!visited.get(point).getFrom().equals(start)) {
             point = visited.get(point).getFrom();
         }
         return visited.get(point).getDirection();
     }
 
-    private static LinkedHashSet<Point> bfs(Board board, Queue<Point> queue, Map<Point, Path> visited, Elements[] barrier, Elements[] target, int max, MODE mode, boolean all) {
+    private LinkedHashSet<Point> bfs(Queue<Point> queue, Map<Point, Path> visited, int max) {
         LinkedHashSet<Point> found = new LinkedHashSet<>();
         while (!queue.isEmpty()) {
             Point point = queue.poll();
 
             if (board.isAt(point, target)) {
                 found.add(point);
-                if (!all) {
+                if (!weight) {
                     break;
                 }
             }
@@ -115,7 +129,7 @@ public class BFS {
                     if (distance > max)
                         continue;
 
-                    if (!isSafe(board, p, mode)) {
+                    if (!isSafe(p)) {
                         continue;
                     }
 
@@ -127,13 +141,52 @@ public class BFS {
         return found;
     }
 
-    private static boolean isSafe(Board board, Point p, MODE mode) {
+    private boolean isSafe(Point point) {
         switch (mode) {
-            case NORMAL: return board.isSafe(p);
-            case FLY: return board.isSafeFly(p);
-            case ATTACK: return board.isSafeAttack(p);
+            case NORMAL: return board.isSafe(point);
+            case FLY: return board.isSafeFly(point);
+            case ATTACK: return board.isSafeAttack(point);
         }
         return true;
+    }
+
+    public static class Builder {
+        private BFS bfs;
+
+        public static Builder newBFS(Board board, Point start) {
+            Builder builder = new Builder();
+            builder.bfs = new BFS(board, start);
+            return builder;
+        }
+
+        public Builder fly() {
+            bfs.mode = MODE.FLY;
+            return this;
+        }
+
+        public Builder attack() {
+            bfs.mode = MODE.ATTACK;
+            return this;
+        }
+
+        public Builder barrier(Elements[] barrier) {
+            bfs.barrier = barrier;
+            return this;
+        }
+
+        public Builder target(Elements[] target) {
+            bfs.target = target;
+            return this;
+        }
+
+        public Builder weight(boolean weight) {
+            bfs.weight = weight;
+            return this;
+        }
+
+        public BFS build() {
+            return bfs;
+        }
     }
 
     private static class Path {
