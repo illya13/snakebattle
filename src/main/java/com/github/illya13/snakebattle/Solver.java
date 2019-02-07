99b5d25be76179676440a9520be301e4a980f0aa
Illya Havsiyevych <illya.havsiyevych@gmail.com>
2019-02-07 17:26:04 +0200

    remove old

--- src/main/java/com/github/illya13/snakebattle/Solver.java
+++ src/main/java/com/github/illya13/snakebattle/Solver.java
@@ -1,28 +1,5 @@
 package com.github.illya13.snakebattle;
 
-/*-
- * #%L
- * Codenjoy - it's a dojo-like platform from developers to developers.
- * %%
- * Copyright (C) 2018 Codenjoy
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
- */
-
-
 import com.codenjoy.dojo.services.*;
 import com.codenjoy.dojo.client.WebSocketRunner;
 import com.codenjoy.dojo.snakebattle.model.Elements;
@@ -32,12 +9,6 @@ import java.util.*;
 
 import static com.codenjoy.dojo.snakebattle.model.Elements.*;
 
-/**
- * User: Illia Gavsiievych
- * Это твой алгоритм AI для игры. Реализуй его на свое усмотрение.
- * Обрати внимание на {@see YourSolverTest} - там приготовлен тестовый
- * фреймворк для тебя.
- */
 public class Solver implements com.codenjoy.dojo.client.Solver<com.codenjoy.dojo.snakebattle.client.Board> {
     private static final String BASE_URL = "https://game2.epam-bot-challenge.com.ua/codenjoy-contest/board/player/";
     private static final String PLAYER_CODE = "?code=1617935781189693616";
