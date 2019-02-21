6b77f3461463728bb77365c4f205bde76cca91a5
Illya Havsiyevych <illya.havsiyevych@gmail.com>
2019-02-21 16:15:56 +0200

    a bit safety

--- src/main/java/com/github/illya13/snakebattle/solver/BFSSolver.java
+++ src/main/java/com/github/illya13/snakebattle/solver/BFSSolver.java
@@ -9,21 +9,25 @@ import com.github.illya13.snakebattle.State;
 
 import java.util.Map;
 
-import static com.github.illya13.snakebattle.Board.BARRIER_ELEMENTS;
+import static com.github.illya13.snakebattle.Board.*;
 
 public class BFSSolver implements Solver {
     @Override
     public Direction next(State state) {
-        Direction direction = findFirst(state, state.board(), Elements.FURY_PILL, Elements.APPLE, Elements.GOLD, Elements.FLYING_PILL);
+        Direction direction = findFirst(state, Elements.FURY_PILL, Elements.APPLE, Elements.GOLD, Elements.FLYING_PILL);
         return (direction != null) ? direction : Direction.RIGHT;
     }
 
-    private Direction findFirst(State state, Board board, Elements... elements) {
+    private Direction findFirst(State state, Elements... elements) {
         int min = Integer.MAX_VALUE;
         Direction direction = null;
 
         for (State.Action action: state.me().actions()) {
-            if (board.isAt(action.direction().change(state.me().head()), BARRIER_ELEMENTS))
+            Point point = action.direction().change(state.me().head());
+            if (unsafeBorder(state.board(), point))
+                continue;
+
+            if (unsafeStone(state, point))
                 continue;
 
             Map<Point, Integer> items = action.items(elements);
@@ -39,5 +43,14 @@ public class BFSSolver implements Solver {
             }
         }
         return direction;
+    }
+
+    private boolean unsafeStone(State state, Point point) {
+        return state.board().isAt(point, Elements.STONE) &&
+                ( (state.me().size() < 5) && !state.me().isFly() && !state.me().isFury());
+    }
+
+    private boolean unsafeBorder(Board board, Point point) {
+        return board.isAt(point, join(BARRIER_ELEMENTS, ENEMY_ELEMENTS));
     }
 }
